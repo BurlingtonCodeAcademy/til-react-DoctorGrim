b578b6aefa5a077dfddc6ec3720adf767cb84e46
DoctorGrim <estlund.marshall@gmail.com>
2019-08-01 22:53:53 -0400

    added basic list of entries for stories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+//import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
+const [data, setData] = useState([0]);
+
+  //use hook to fetch from /facts setDate sets the state for data
+ useEffect( () => {
+   const getData = async ()=>{
+    // fetch returns a promise. If you are not familiar with promises, see
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+     await fetch('/facts')
+      .then(data => data.json())
+      .then((res) => {
+        console.log(res)
+       setData(res);
+       
+      });
+    }
+    getData();
+  },[]);
+
+
+  
+  console.log(data)
+  
+  
   return (
     <div className="App">
       <h1>Today I Learned</h1>
@@ -13,6 +37,17 @@ function App() {
         <input type="text" name="text" />
         <input type="submit" />
       </form>
+
+
+{data.map((data, index) =>
+  <div key={data.name + index}>
+      <h1>{data.when}</h1>
+      <p>{data.text}</p>
+      <hr/>
+  </div>
+  )}
+
+      <p>{data[0].text}</p>
     </div>
   );
 }
